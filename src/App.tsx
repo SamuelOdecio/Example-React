226be330067b83088599a61701fbc7dbbeed3b14
Samuel Odecio <108842146+SamuelOdecio@users.noreply.github.com>
2023-02-23 20:19:41 -0400

    Initial Comit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+import { Button } from './components/Button'
+import { Title } from './components/Title'
+import { Home } from './pages/Home'
+import { Login } from './pages/Login'
+
+function App() {
+  return (
+    <div id="App">
+      <Login/>
+    </div>
+  )
+}
+
+export default App
